54cc9d03c996ae126a5c15c4d637187b50e6da69
Peter Li <pl.climbs@gmail.com>
2024-04-07 02:50:59 -0700

    Add my first multithreaded server

---
+++ my-server/src/lib.rs
@@ -0,0 +1,64 @@
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+#[derive(Debug)]
+struct WorkerCreationError;
+
+impl Worker {
+    pub fn build(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Result<Worker, WorkerCreationError> {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {id} got a job; executing...");
+            job();
+        });
+
+        Ok(Worker { id, thread })
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+#[derive(Debug)]
+pub struct PoolCreationError;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `build` function will panic if the size is zero.
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::build(id, Arc::clone(&receiver)).unwrap());
+        }
+
+        Ok(ThreadPool { workers, sender })
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
---
+++ my-server/src/main.rs
@@ -0,0 +1,73 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+use log::{info};
+use my_server::ThreadPool;
+
+fn main() {
+    env_logger::init();
+
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::build(4).unwrap();
+
+    info!("Starting server");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let success_html = r#"
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Hello!</title>
+        </head>
+        <body>
+            <h1>Hello!</h1>
+            <p>Hi from Rust</p>
+        </body>
+    </html>
+    "#;
+
+    let not_found_html = r#"
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Hello!</title>
+        </head>
+        <body>
+            <h1>Oops!</h1>
+            <p>Sorry, I don't know what you're asking for.</p>
+        </body>
+    </html>
+    "#;
+
+
+    let (status_line, contents) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", success_html),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", success_html)
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", not_found_html),
+    };
+
+    let length = contents.len();
+    let length_line = format!("Content-Length: {length}");
+    let response = format!("{status_line}\r\n{length_line}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+    info!("{request_line} -> {status_line}");
+}
